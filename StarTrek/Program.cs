50006b9246ddaba76dda4483da23c5ad17e91bb9
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-01-13 11:44:14 +0100

    forsøg

--- StarTrek/Program.cs
+++ StarTrek/Program.cs
@@ -11,14 +11,10 @@ namespace StarTrek
             {
                 List<string> vulcanMaleNames = new List<string>();
                 List<string> vulcanFemaleNames = new List<string>();
-                string[] allowedVulcanMaleNameCharacters = { "S", "Sp ", "Sk", "St", "T" };
-                string[] allowedVulcanMaleNameCharacters2 = { "a", "e", "i", "o", "u", "y" };
-                string[] allowedVulcanMaleNameCharacters3 = { "r", "t", "p", "d", "f", "j", "k", "l", "v", "b", "n", "m" };
-                string[] allowedVulcanMaleNameCharacters4 = { "a", "e", "i", "o", "u", "y" };
-                string[] allowedVulcanMaleNameCharacters5 = { "q", "p", "k", "ck", "l" };
+                string[] allowedVulcanMaleNameCharacters = { "S,Sp,Sk,St,T", "a,e,i,o,u,y", "r,t,p,d,f,j,k,l,v,b,n,m", "a,e,i,o,u,y",  "q,p,k,ck,l" };
                 Console.Write("Indtast et navn: ");
                 string input = Console.ReadLine();
-                if(input == allowedVulcanMaleNameCharacters[0] + allowedVulcanMaleNameCharacters2[0] + allowedVulcanMaleNameCharacters3[0] + allowedVulcanMaleNameCharacters4[0] + allowedVulcanMaleNameCharacters5[0])
+                if(input == String.Concat(allowedVulcanMaleNameCharacters[0].Split(","), allowedVulcanMaleNameCharacters[1].Split(","), allowedVulcanMaleNameCharacters[2].Split(","), allowedVulcanMaleNameCharacters[3].Split(","), allowedVulcanMaleNameCharacters[4].Split(",")))
                 {
                     Console.WriteLine("Dette er et lovligt vulcan male navn");
                 }
